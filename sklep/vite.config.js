1c386376262646296c85be9b4ea5e8b72007b75c
michalbuszka <318049@stud.umk.pl>
2025-08-29 23:04:08 +0200

    init v2

---
+++ sklep/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // automatyczna aktualizacja SW
+      manifest: {
+        name: 'My React PWA',
+        short_name: 'PWA',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#0f172a',
+        icons: [
+          { src: 'android-launchericon-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'android-launchericon-512x512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
+});
